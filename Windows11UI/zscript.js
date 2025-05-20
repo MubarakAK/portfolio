7c41cb57541b941656fd9d9c7b4e88d93c66653c
MubarakAK <127643785+MubarakAK@users.noreply.github.com>
2025-05-20 19:05:09 +0530

    Add files via upload

---
+++ Windows11UI/zscript.js
@@ -0,0 +1,27 @@
+let taskbar = document.getElementsByClassName("footer1")[0]
+let startmenu = document.getElementsByClassName("start")[0]
+let bg = document.getElementsByClassName("bg")[0]
+let no = 1
+taskbar.addEventListener("click", ()=>{
+    console.log("clicked")
+    if(startmenu.style.bottom == '-70vh')
+    {
+        startmenu.style.bottom = '6vh'
+    }
+    else{
+        startmenu.style.bottom = '-70vh'
+    }
+})
+
+setInterval(() => {
+    bg.style.backgroundImage = `url(Images/${no}.jpg)`
+    
+    if(no==2){
+        no =1
+    }
+    else{
+        no = 2
+    }
+
+
+}, 5000);
