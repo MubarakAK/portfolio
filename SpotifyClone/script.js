de07712e087f5005a76a34c73cbba86071fd2242
MubarakAK <127643785+MubarakAK@users.noreply.github.com>
2025-07-02 21:56:39 +0530

    Update script.js

--- SpotifyClone/script.js
+++ SpotifyClone/script.js
@@ -1,5 +1,5 @@
-console.log("hi quantify")
 let audioelem = new Audio("/audios/4.mp3");
+console.log("hi quantify", audioelem)
 const currentUrl = window.location.href;
 let server = currentUrl ;
 let playbutseek = document.getElementsByClassName("icplay")[0];
